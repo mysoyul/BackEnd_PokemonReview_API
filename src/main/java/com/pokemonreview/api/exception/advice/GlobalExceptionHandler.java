283e4a0821739f3f0ccbe7df8a1ec5663675ce5b
mysoyul <vega2k_@naver.com>
2024-12-04 16:46:33 +0900

    Advice ProbleDetail 사용

--- src/main/java/com/pokemonreview/api/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/pokemonreview/api/exception/advice/GlobalExceptionHandler.java
@@ -15,16 +15,29 @@ import java.time.Instant;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+    /*
+        Spring6 버전에 추가된 ProblemDetail 객체에 에러 정보를 담아서 리턴하는 방법
+     */
     @ExceptionHandler(MyResourceException.class)
-    public ResponseEntity<ErrorObject> handleResourceNotFoundException(MyResourceException ex) {
-        ErrorObject errorObject = new ErrorObject();
-        errorObject.setStatusCode(ex.getStatusCode());
-        errorObject.setMessage(ex.getMessage());
-
-        log.error(ex.getMessage(), ex);
-
-        return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(ex.getStatusCode()));
+    protected ProblemDetail handleException(MyResourceException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(e.getStatusCode());
+        problemDetail.setTitle("Not Found");
+        problemDetail.setDetail(e.getMessage());
+        problemDetail.setProperty("errorCategory", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
     }
+
+//    @ExceptionHandler(MyResourceException.class)
+//    public ResponseEntity<ErrorObject> handleResourceNotFoundException(MyResourceException ex) {
+//        ErrorObject errorObject = new ErrorObject();
+//        errorObject.setStatusCode(ex.getStatusCode());
+//        errorObject.setMessage(ex.getMessage());
+//
+//        log.error(ex.getMessage(), ex);
+//
+//        return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(ex.getStatusCode()));
+//    }
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<ErrorObject> handleException(RuntimeException e) {
