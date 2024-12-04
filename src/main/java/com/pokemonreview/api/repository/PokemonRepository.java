4f1cb99170e6d0f491968b583f69323617425251
mysoyul <vega2k_@naver.com>
2024-12-04 14:09:15 +0900

    PokemonRepository add

---
+++ src/main/java/com/pokemonreview/api/repository/PokemonRepository.java
@@ -0,0 +1,13 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.PokemonType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
+    Optional<Pokemon> findByType(PokemonType type);
+    List<Pokemon> findByName(String name);
+}
