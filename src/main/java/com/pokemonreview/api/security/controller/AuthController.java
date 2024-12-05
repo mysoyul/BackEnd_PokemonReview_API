0c192cf122aed06d00aefed199b872bbd67505db
mysoyul <vega2k_@naver.com>
2024-12-05 10:22:29 +0900

    AuthController login method add

--- src/main/java/com/pokemonreview/api/security/controller/AuthController.java
+++ src/main/java/com/pokemonreview/api/security/controller/AuthController.java
@@ -1,33 +1,62 @@
 package com.pokemonreview.api.security.controller;
 
+import com.pokemonreview.api.security.dto.AuthResponseDto;
+import com.pokemonreview.api.security.dto.LoginDto;
 import com.pokemonreview.api.security.dto.UserDto;
+import com.pokemonreview.api.security.jwt.JWTGenerator;
 import com.pokemonreview.api.security.models.RoleEntity;
 import com.pokemonreview.api.security.models.UserEntity;
 import com.pokemonreview.api.security.repository.RoleRepository;
 import com.pokemonreview.api.security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
 
-		public AuthController(UserRepository userRepository,
-                              RoleRepository roleRepository,
-                              PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JWTGenerator jwtGenerator;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        //token 생성
+        String token = jwtGenerator.generateToken(authentication);
+
+        AuthResponseDto authResponseDTO = new AuthResponseDto(token);
+        authResponseDTO.setUsername(loginDto.getUsername());
+
+        Optional<UserEntity> optionalUser =
+                userRepository.findByUsername(loginDto.getUsername());
+        if(optionalUser.isPresent()){
+            UserEntity userEntity = optionalUser.get();
+            authResponseDTO.setRole(userEntity.getRoles().get(0).getName());
+        }
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
     @GetMapping("/welcome")
