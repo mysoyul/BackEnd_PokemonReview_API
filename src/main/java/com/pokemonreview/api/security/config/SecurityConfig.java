0340e719813d4942f98476f4b6799fb089912944
mysoyul <vega2k_@naver.com>
2024-12-05 11:15:37 +0900

    SecurityConfig filter/errorHandler 설정

--- src/main/java/com/pokemonreview/api/security/config/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/security/config/SecurityConfig.java
@@ -1,21 +1,30 @@
 package com.pokemonreview.api.security.config;
 
+import com.pokemonreview.api.security.exception.CustomAccessDeniedHandler;
+import com.pokemonreview.api.security.exception.JwtAuthEntryPoint;
+import com.pokemonreview.api.security.jwt.filter.JWTAuthenticationFilter;
 import com.pokemonreview.api.security.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
 
@@ -34,6 +43,24 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    //Token 을 검증하는 필터를 Bean 으로 설정
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter() {
+        return new JWTAuthenticationFilter();
+    }
+
+    //권한오류(403)를 처리하는 핸들러를 Bean 으로 설정
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new CustomAccessDeniedHandler();
+    }
+
+    //인증오류(401)를 처리하는 핸들러를 Bean 으로 설정
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return new JwtAuthEntryPoint();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 				return http
@@ -43,7 +70,13 @@ public class SecurityConfig {
                             auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/pokemon/**","/api/admin/**").authenticated();
                          })
-                         .formLogin(withDefaults())
-                         .build();
+                        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                        .exceptionHandling(authManager -> authManager
+                                .authenticationEntryPoint(authenticationEntryPoint())
+                                .accessDeniedHandler(accessDeniedHandler())
+                        )
+                         //.formLogin(withDefaults())
+                        .build();
     }
 }
